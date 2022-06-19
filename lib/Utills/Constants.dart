b14a2c1be13d27b0c29ef5288b3062738a7fe844
Rushabh Navadiya <55693332+rushabhnavadiya@users.noreply.github.com>
2022-06-19 21:52:10 +0530

    Update Constants.dart

--- lib/Utills/Constants.dart
+++ lib/Utills/Constants.dart
@@ -9,14 +9,14 @@ class Constants{
   static const  String ios_interstitial_id = '714897359396761_714907116062452';
 
 
-  static const  String android_app_link = 'https://play.google.com/store/apps/details?id=com.manthanvanani.cmreward';
+  static const  String android_app_link = 'https://play.google.com/store/apps/details?id=com.rushabhnavadiya.cmreward';
   static const  String ios_app_link = 'https://itunes.apple.com/us/app/myapp/id1552014627';
-  static const  String android_app = 'com.manthanvanani.cmreward';
+  static const  String android_app = 'com.rushabhnavadiya.cmreward';
   static const  String ios_app = '1552014627';
-  static const  String email_id = 'manthanvannai9442@gmail.com';
+  static const  String email_id = 'rushabhnavadiya1998@gmail.com';
   static const  String email_subject = 'Suggestion';
   static const  String android_apps = 'https://apps.apple.com/us/developer/manthan-vanani/id1510374545';
-  static const  String ios_apps = 'https://play.google.com/store/apps/developer?id=Manthan+Vanani';
+  static const  String ios_apps = 'https://play.google.com/store/apps/developer?id=Rushabh+Navadiya';
 
   static const  String last_update_date = 'last_update_date';
   // '#7339E5'
